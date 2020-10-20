a8bc3b6554cd07e79bdd06248a3d684ed13868f5
Cong Le Thanh <cong.lee2025@gmail.com>
2020-10-20 23:22:44 +0700

    create splash screen

---
+++ app/src/main/java/com/example/travelutilityapp/SplashActivity.java
@@ -0,0 +1,67 @@
+package com.example.travelutilityapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation topAnim, botAnim;
+    ImageView logo;
+    TextView slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsets.Type.statusBars());
+            }
+        } else {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            );
+        }
+
+        // load animation
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        // hooks
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.slogan);
+
+        // set animations for objects
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(botAnim);
+
+        // delete splash activity from queue and display home activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+    }
+
+
+}
